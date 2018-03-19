585171a87fe8518bb426269878fefa4f16fe223b
Kostis Galoussis <konstantinos.galoussis@agileactors.com>
2018-03-19 22:46:10 +0200

    test meta commit to all

---
+++ postcss.config.js
@@ -0,0 +1,9 @@
+module.exports = ctx => ({
+  // parser: ctx.parser ? 'sugarss' : false,
+  // map: ctx.env === 'development' ? ctx.map : false,
+  // from: ctx.from,
+  // to: ctx.to,
+  // plugins: {
+  //   'postcss-plugin': ctx.plugin,
+  // },
+});
